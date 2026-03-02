cb40e3c3b3a403439ce207eea7f5e7ca23796aba
Zach Perry <zachrperry@outlook.com>
2026-03-01 22:53:23 -0500

    add vercel speedinsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
 
 const _geist = Geist({ subsets: ["latin"] });
@@ -112,6 +113,7 @@ export default function RootLayout({
       <body className="font-sans antialiased">
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
